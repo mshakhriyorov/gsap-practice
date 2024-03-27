3a8b9aa1e79e16ac4b0dac0d41408a2123193a4b
mshakhriyorov <mshakhriyorov8@gmail.com>
2024-03-27 16:58:12 +0500

    Update UI elements in GsapMain.vue
    The visualization elements have been improved in the GsapMain.vue component. The layout of images has been updated with new styles and wrappers for better positioning. Also, two new circular div elements have been added for enhanced aesthetics. These changes aim to make the digital presence of restaurants more visually appealing.

--- components/GsapMain.vue
+++ components/GsapMain.vue
@@ -1,16 +1,28 @@
 <template>
-  <div ref="containerRef" class="py-12 px-30" @mousemove="onMouseMove">
+  <div ref="containerRef" class="py-12 px-30 relative" @mousemove="onMouseMove">
     <div class="grid grid-cols-3 min-w-[1200px]">
       <h2 class="flex flex-col items-start text-[#FF5100] font-semibold text-[50px] min-w-[530px]">TAOMLA
         <span class="capitalize text-[30px] text-[#170A04]">making your restaurant's digital presence stand out</span>
       </h2>
 
-      <img src="/pizza.png" alt="pointer" class="item w-[163px] col-span-2 place-self-center">
-      <img src="/cake.png" alt="pointer" class="item w-[255px] self-end" />
-      <img src="/bread.png" alt="pointer" class="item w-[255px] self-start " />
-      <img src="/burger.png" alt="pointer" class="item w-[438px]" />
-      <img src="/salad.png" alt="pointer" class="item w-[163px] col-span-2 place-self-center" />
+      <div class="item col-span-2 place-self-center">
+        <img src="/pizza.png" alt="pointer" class="w-[163px]">
+      </div>
+      <div class="item self-end">
+        <img src="/cake.png" alt="pointer" class="w-[255px]">
+      </div>
+      <div class="item self-start">
+        <img src="/bread.png" alt="pointer" class="w-[255px]">
+      </div>
+      <div class="item">
+        <img src="/burger.png" alt="pointer" class="w-[438px]">
+      </div>
+      <div class="item col-span-2 place-self-center">
+        <img src="/salad.png" alt="pointer" class="w-[163px]">
+      </div>
     </div>
+    <div class="circle right"/>
+    <div class="circle left"/>
   </div>
 </template>
 
@@ -61,5 +73,59 @@ onMounted(() => {
 body {
   width: 100%;
   height: 100vh;
+}
+
+.circle {
+  @apply rounded-full w-8 h-8 [background:#FF5100] absolute;
+
+  &.left {
+    @apply left-40  bottom-20;
+  }
+
+
+  &.right {
+    @apply right-40  bottom-4;
+  }
+}
+
+.item {
+  @apply relative;
+
+  &::before, &::after {
+    @apply content-[''] [background:#FF5100] absolute rounded-full;
+  }
+
+  &:nth-of-type(1)::before {
+    @apply w-[30px] h-[30px] right-[-50px] -top-2.5;
+  }
+
+  &:nth-of-type(1)::after {
+    @apply w-5 h-5 -left-2.5 -bottom-2.5;
+  }
+
+
+  &:nth-of-type(2)::before {
+    @apply w-[30px] h-[30px] left-[-50px] -top-2.5;
+  }
+
+  &:nth-of-type(2)::after {
+    @apply w-5 h-5 -right-10 bottom-2.5;
+  }
+
+  &:nth-of-type(3)::before {
+    @apply w-[30px] h-[30px] left-0 -top-2.5;
+  }
+
+  &:nth-of-type(3)::after {
+    @apply w-[50px] h-[50px] right-[50px] bottom-2.5;
+  }
+
+  &:nth-of-type(4)::before {
+    @apply w-2.5 h-2.5 -left-2.5 -bottom-2.5;
+  }
+
+  &:nth-of-type(4)::after {
+    @apply w-10 h-10 -right-2.5 -bottom-2.5;
+  }
 }
 </style>
